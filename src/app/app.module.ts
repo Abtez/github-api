a5901c51799d616037026c42d49383607452e5e3
Abzed <ibraabzed@gmail.com>
2020-08-29 13:10:59 +0300

    add navbar and user class component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { GithubProfileComponent } from './github-profile/github-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    GithubProfileComponent,
   ],
   imports: [
     BrowserModule,
